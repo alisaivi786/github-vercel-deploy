9d08208d83e90190935590c1c32310b7640fa88d
aliraza <aliraza@DESKTOP-SDMAHMT>
2024-04-01 11:06:53 +0400

    update 41

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,6 +22,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('this is new features');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('this is a new feature!');
   });
 });
